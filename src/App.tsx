9054edc4b790bfd7e37822409ec125adc720fcca
hwangjinseong <imhjs2023@gmail.com>
2022-11-16 09:57:19 +0900

    파일세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from 'components/main';
+import Login from 'components/login';
+import SignUp from 'components/login/signup';
 import StyleProvider from 'styles';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
      <BrowserRouter>
        <Routes>
         <Route path='/' element={<Main/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/signup' element={<SignUp/>}/>
        </Routes>
      </BrowserRouter>
     </StyleProvider>
